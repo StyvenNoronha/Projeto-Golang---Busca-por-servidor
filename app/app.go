6caa2afb276bce8cfa716117404f425a72891416
styven <styvenn16@gmail.com>
2024-11-08 09:26:09 -0300

    Criando a funcionalidade de busca por IP

--- app/app.go
+++ app/app.go
@@ -1,11 +1,48 @@
 package app
 
-import "github.com/urfave/cli"
-//Gear vai retorna a aplicação na linha de comando 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+//Gear vai retorna a aplicação na linha de comando
+
+func buscarIps(c *cli.Context){
+	host := c.String("host")
+
+	ips,erro:= net.LookupIP(host)
+	if erro != nil{
+		log.Fatal(erro)
+	}
+
+	for _,ip := range ips{
+		fmt.Println(ip)
+	}
+}
+
+
 func Gerar() *cli.App{
 	app:= cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e Nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:"ip",
+			Usage: "Busca por Ip endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "google.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
 }
+
--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 )
 
 func main(){
+	/*
+	so executa na linha de comando
+	go run main.go ip --host pudim.com.br
+
+	se colocar sem a flag --host. Volta o ip do google
+	*/
 	fmt.Println("Inicio")
 
 	aplicacao:= app.Gerar()
 	erro := aplicacao.Run(os.Args)
-	
+
 	if erro != nil{
 		log.Fatal(erro)
 	}
